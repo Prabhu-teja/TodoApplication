178c71e9a86e6b2eda3a861022350f8e8cf178a5
Prabhu_teja <53848058+Prabhu-teja@users.noreply.github.com>
2020-12-02 15:50:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+if(localStorage.length==0){
+    localStorage.setItem("index",0)
+}
+document.querySelector("#clear").addEventListener("click",()=>{
+    if(confirm("Are you sure?")){
+        localStorage.clear();
+        localStorage.setItem("index",0);
+        display();
+    }
+});
+function display(){
+    let container=document.querySelector("#todos");
+    container.innerHTML="";
+    for(let i=1;i<localStorage.length;i++){
+        let div=document.createElement("div");
+        div.className="todo m-2 p-2";
+        div.innerText=localStorage.getItem(i);
+        container.appendChild(div);
+    }
+}
+document.querySelector("#save").addEventListener("click",()=>{
+    let message=document.getElementById("message");
+    localStorage.setItem(parseInt(localStorage.getItem("index"))+1,message.value);
+    localStorage.setItem("index",parseInt(localStorage.getItem("index"))+1);
+    message.value="";
+    display();
+});
+if(localStorage.length>1){
+    display();
+}
+document.getElementById("search").addEventListener("keyup",()=>{
+    let searchKey=document.getElementById("search").value.toLowerCase();
+    let todos=document.getElementsByClassName("todo");
+    for(let i=0;i<todos.length;i++){
+        let msg=todos[i].innerText.toLowerCase();
+        if(msg.indexOf(searchKey)>=0){
+            todos[i].style.display="inherit";
+        }
+        else{
+            todos[i].style.display="none";
+        }
+    }
+})
